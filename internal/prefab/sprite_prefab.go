510e472244db15b5d93a95208a7885e60208450a
jiepengtan <jiepengtan@gmail.com>
2024-05-30 10:27:17 +0800

    add sprite prefab

---
+++ internal/prefab/sprite_prefab.go
@@ -0,0 +1,36 @@
+package prefab
+
+import "github.com/goplus/spx/internal/math32"
+
+type SpritePrefabData struct {
+	Name      string           `json:"Name"`
+	Hierarchy []HierarchyData  `json:"Hierarchy"`
+	SkinMesh  []SpriteSkinData `json:"SkinMesh"`
+}
+
+type HierarchyData struct {
+	Name   string         `json:"Name"`
+	PosRot math32.Vector3 `json:"PosRot"`
+	Order  int            `json:"Order"`
+	Parent string         `json:"Parent"`
+}
+
+type SpriteSkinData struct {
+	Name           string           `json:"Name"`
+	Indices        []uint16         `json:"Indices"`
+	Vertices       []math32.Vector3 `json:"Vertices"`
+	BoneWeights    []BoneWeight     `json:"BoneWeights"`
+	BindPoses      []math32.Matrix4 `json:"BindPoses"`
+	BoneTransforms []string         `json:"BoneTransforms"`
+}
+
+type BoneWeight struct {
+	Weight0    float64 `json:"m_Weight0"`
+	Weight1    float64 `json:"m_Weight1"`
+	Weight2    float64 `json:"m_Weight2"`
+	Weight3    float64 `json:"m_Weight3"`
+	BoneIndex0 int     `json:"m_BoneIndex0"`
+	BoneIndex1 int     `json:"m_BoneIndex1"`
+	BoneIndex2 int     `json:"m_BoneIndex2"`
+	BoneIndex3 int     `json:"m_BoneIndex3"`
+}
