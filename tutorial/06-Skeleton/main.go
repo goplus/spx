77d76571a3e4383d9f5ee8a3eba35b8f1791f0b9
jiepengtan <jiepengtan@gmail.com>
2024-05-30 14:31:43 +0800

    add skeleton tutorial demo

---
+++ tutorial/06-Skeleton/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"image/color"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/goplus/spx/internal/skeleton"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+	scale        = 64
+	starsCount   = 1024
+)
+
+type Vertex struct {
+	x, y float32
+}
+
+func (s *Vertex) Draw(screen *ebiten.Image) {
+	c := color.RGBA{
+		R: uint8(0xbb * 128 / 0xff),
+		G: uint8(0xdd * 128 / 0xff),
+		B: uint8(0xff * 128 / 0xff),
+		A: 0xff}
+	vector.StrokeLine(screen, s.x, s.y, s.x+3, s.y+3, 1, c, true)
+}
+
+type Game struct {
+	prefabData skeleton.SpritePrefabData
+	animData   skeleton.SpriteAnimData
+	verteies   [starsCount]Vertex
+	bones      [starsCount]Vertex
+}
+
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for i := 0; i < starsCount; i++ {
+		//g.verteies[i].Draw(screen)
+	}
+	for i := 0; i < starsCount; i++ {
+		g.bones[i].Draw(screen)
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func (g *Game) CreateCharacter() {
+	{
+		data, err := ioutil.ReadFile("./assets/sprites/Tom/animation/run.json")
+		if err != nil {
+			fmt.Println("File reading error", err)
+			return
+		}
+
+		err = json.Unmarshal(data, &g.animData)
+		if err != nil {
+			fmt.Println("Error parsing JSON", err)
+			return
+		}
+	}
+	{
+		data, err := ioutil.ReadFile("./assets/sprites/Tom/prefab.json")
+		if err != nil {
+			fmt.Println("File reading error", err)
+			return
+		}
+
+		err = json.Unmarshal(data, &g.prefabData)
+		if err != nil {
+			fmt.Println("Error parsing JSON", err)
+			return
+		}
+	}
+	scale := 30.0
+	meshes := g.prefabData.SkinMesh
+	idx := 0
+	for i := 0; i < len(meshes); i++ {
+		vertices := meshes[i].Vertices
+		for j := 0; j < len(vertices); j++ {
+			g.verteies[idx].x = float32(vertices[j].X*scale) + screenWidth*0.5
+			g.verteies[idx].y = float32(vertices[j].Y*scale) + screenHeight*0.5
+			idx++
+		}
+	}
+	skeleton := skeleton.BuildSkeleton(g.prefabData.Hierarchy)
+	bones := skeleton.Bones
+	for i := 0; i < len(bones); i++ {
+		pos := bones[i].Pos
+		g.bones[i].x = float32(pos.X*scale) + screenHeight*0.5
+		g.bones[i].y = float32(pos.Y*scale) + screenHeight*0.5
+	}
+
+}
+
+func main() {
+	game := NewGame()
+	game.CreateCharacter()
+
+	rand.Seed(time.Now().UnixNano())
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Stars (Ebitengine Demo)")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
