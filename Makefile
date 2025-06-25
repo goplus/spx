d81665119da7108ccac811e1cb0272389d1913e1
jiepengtan <jiepengtan@gmail.com>
2025-06-25 10:29:48 +0800

    Update dirs

--- Makefile
+++ Makefile
@@ -94,8 +94,6 @@ pc:
 # Build web template
 web: 
 	./pkg/gdspx/tools/build_engine.sh -p web &&\
-	cp -rf /Users/tjp/projects/robot/spx/pkg/gdspx/godot/bin/godot.web.template_debug.wasm32.nothreads.wasm /Users/tjp/projects/robot/godot-love-wechat/export/engine/engine.wasm &&\
-	cp -rf /Users/tjp/projects/robot/spx/cmd/igox/gdspx.wasm /Users/tjp/projects/robot/godot-love-wechat/export/engine/gdspx.wasm &&\
 	./pkg/gdspx/tools/make_util.sh extrawebtemplate 
 
 # Build android template
--- cmd/gox/template/project/.builds/minigame/js/loader.js
+++ cmd/gox/template/project/.builds/minigame/js/loader.js
@@ -1,3 +1,5 @@
+import "weapp-adapter";
+import "fetch";
 // 首先设置 crypto polyfill，必须在导入 Godot 编辑器之前
 const crypto = {
   getRandomValues: (view) => {
@@ -9,6 +11,52 @@ const crypto = {
   },
 };
 
+if (!globalThis.TextEncoder) {
+  globalThis.TextEncoder = class {
+    encode(string) {
+      const utf8 = [];
+      for (let i = 0; i < string.length; i++) {
+        let charcode = string.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+          utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+          utf8.push(0xe0 | (charcode >> 12), 0x80 | ((charcode>>6) & 0x3f), 0x80 | (charcode & 0x3f));
+        }
+        else {
+          i++;
+          charcode = 0x10000 + (((charcode & 0x3ff)<<10) | (string.charCodeAt(i) & 0x3ff));
+          utf8.push(0xf0 | (charcode >>18), 0x80 | ((charcode>>12) & 0x3f), 0x80 | ((charcode>>6) & 0x3f), 0x80 | (charcode & 0x3f));
+        }
+      }
+      return new Uint8Array(utf8);
+    }
+  };
+}
+
+if (!globalThis.TextDecoder) {
+  globalThis.TextDecoder = class {
+    decode(bytes) {
+      let string = "";
+      let i = 0;
+      while (i < bytes.length) {
+        let c = bytes[i++];
+        if (c > 127) {
+          if (c > 191 && c < 224) {
+            c = (c & 31) << 6 | bytes[i++] & 63;
+          } else if (c > 223 && c < 240) {
+            c = (c & 15) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
+          } else if (c > 239 && c < 248) {
+            c = (c & 7) << 18 | (bytes[i++] & 63) << 12 | (bytes[i++] & 63) << 6 | bytes[i++] & 63;
+          }
+        }
+        string += String.fromCharCode(c);
+      }
+      return string;
+    }
+  };
+}
 // 立即设置到 globalThis 上，确保 Godot 编辑器导入时能找到
 globalThis.crypto = crypto;
 
@@ -19,8 +67,8 @@ console.log("globalThis.crypto", globalThis.crypto)
 let GodotSDK;
 async function loadModules() {
   await import("./fetch.js"); // 先加载 fetch polyfill
-  await import("./js/engine");
-  const sdkModule = await import("./js/sdk");
+  await import("./engine");
+  const sdkModule = await import("./sdk");
   GodotSDK = sdkModule.GodotSDK;
 }
 
