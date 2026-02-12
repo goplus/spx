40ecf5b4cb0b752acc2fdb1e89376940190eb1f5
joeykchen <466719968@qq.com>
2026-02-12 18:07:36 +0800

    fix: ispx generate code before building

--- cmd/ispx/build.sh
+++ cmd/ispx/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+# Generate code before building
+(cd ../../pkg/ispx && go generate)
+
+# Build the WASM binary
 GOOS=js GOARCH=wasm go build -trimpath -ldflags "-s -w -checklinkname=0" -o ispx.wasm
