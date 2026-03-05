8e575fa9578bbc030c99f55abd18632f651b9e16
joeykchen <466719968@qq.com>
2026-03-05 15:04:58 +0800

    refactor(engine): merge math_Vec2.go into math.go and delete redundant file

--- pkg/gdspx/pkg/engine/math.go
+++ pkg/gdspx/pkg/engine/math.go
@@ -2,6 +2,8 @@ package engine
 
 import (
 	"math"
+
+	mathf "github.com/goplus/spbase/mathf"
 )
 
 func Abs(x float64) float64 {
@@ -14,4 +16,21 @@ func Sign(x float64) int64 {
 		return 1
 	}
 	return 0
+}
+
+func DegToRad(degrees float64) float64 {
+	return degrees * (math.Pi / 180.0)
+}
+
+func RadToDeg(radians float64) float64 {
+	return radians * (180.0 / math.Pi)
+}
+
+func AngleToPoint(v mathf.Vec2, v2 mathf.Vec2) float64 {
+	return Angle(v.Sub(v2))
+}
+
+// Angle returns the angle of v in radians, measured from the positive X axis.
+func Angle(v mathf.Vec2) float64 {
+	return float64(mathf.Atan2(float64(v.Y), float64(v.X)))
 }
