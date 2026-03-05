4bd8daa50b8e216325ac798b41d7001cf19f0a8f
joeykchen <466719968@qq.com>
2026-03-05 11:56:50 +0800

    refactor: move coordinate system-related functions to engine

--- internal/engine/util.go
+++ internal/engine/util.go
@@ -115,3 +115,25 @@ func RadToDeg(radians float64) float64 {
 func Sincos(rad float64) Vec2 {
 	return NewVec2(math.Sincos(rad))
 }
+
+func F64Tof32(slice []float64) []float32 {
+	if slice == nil {
+		return []float32{}
+	}
+	out := make([]float32, len(slice))
+	for i, v := range slice {
+		out[i] = float32(v)
+	}
+	return out
+}
+
+func F32Tof64(slice []float32) []float64 {
+	if slice == nil {
+		return []float64{}
+	}
+	out := make([]float64, len(slice))
+	for i, v := range slice {
+		out[i] = float64(v)
+	}
+	return out
+}
