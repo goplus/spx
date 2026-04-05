979ee7c3ee85ea3f86f12b65f74c7210635e1d83
joeykchen <466719968@qq.com>
2026-04-05 11:49:50 +0800

    fix: correct DelaySpriteCall typo (#1449)

--- pkg/spx/pkg/engine/timer.go
+++ pkg/spx/pkg/engine/timer.go
@@ -4,6 +4,6 @@ func DelayCall(delay float64, callback func()) {
 	requireRuntimeBridge().DelayCall(delay, callback)
 }
 
-func DealySpriteCall(delay float64, sprite ISpriter, callback func()) {
+func DelaySpriteCall(delay float64, sprite ISpriter, callback func()) {
 	requireRuntimeBridge().DelaySpriteCall(delay, sprite, callback)
 }
