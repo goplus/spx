bc068f4f0002349d777a73a90e274c5ebfbdc3c7
joeykchen <466719968@qq.com>
2026-04-09 09:35:09 +0800

    refactor: fold codegen into root module refactor: move codegen license helper into package chore: keep codegen module self-contained chore: preserve license headers in generated Go code chore: add Apache 2.0 headers to Go files

--- internal/cmd/codegen/generate/common/funcs.go
+++ internal/cmd/codegen/generate/common/funcs.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 The XGo Authors (xgo.dev). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package common
 
 import (
@@ -13,6 +29,7 @@ import (
 	"unicode"
 
 	"github.com/goplus/spx/v2/internal/cmd/codegen/gdextensionparser/clang"
+	"github.com/goplus/spx/v2/internal/cmd/codegen/internal/licenseheader"
 	spxlog "github.com/goplus/spx/v2/internal/cmd/codegen/internal/log"
 
 	"github.com/iancoleman/strcase"
@@ -703,6 +720,10 @@ func GenerateFile(funcs template.FuncMap, name string, text string, data any, ds
 	if err != nil {
 		return err
 	}
+	output := b.Bytes()
+	if filepath.Ext(dstPath) == ".go" {
+		output = licenseheader.AddToGoSource(output)
+	}
 
 	dir := filepath.Dir(dstPath)
 	os.MkdirAll(dir, os.ModePerm)
@@ -710,7 +731,7 @@ func GenerateFile(funcs template.FuncMap, name string, text string, data any, ds
 	if err != nil {
 		return err
 	}
-	f.Write(b.Bytes())
+	f.Write(output)
 	f.Close()
 	exec.Command("go", "fmt", dstPath).Run()
 	exec.Command("goimports", "-w", dstPath).Run()
---
+++ internal/cmd/qexpwrap/main.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2021 The XGo Authors (xgo.dev). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/goplus/spx/v2/internal/base/licenseheader"
+)
+
+func main() {
+	args := os.Args[1:]
+	cmd := exec.Command("go", append([]string{"tool", "qexp"}, args...)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = "."
+	if err := cmd.Run(); err != nil {
+		os.Exit(cmd.ProcessState.ExitCode())
+	}
+
+	outdir, ok := flagValue(args, "-outdir")
+	if !ok || outdir == "" {
+		return
+	}
+	if err := addHeadersInDir(outdir); err != nil {
+		_, _ = os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+}
+
+func addHeadersInDir(root string) error {
+	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || filepath.Ext(path) != ".go" {
+			return nil
+		}
+		return licenseheader.AddToGoFile(path)
+	})
+}
+
+func flagValue(args []string, name string) (string, bool) {
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		if arg == name {
+			if i+1 >= len(args) {
+				return "", false
+			}
+			return args[i+1], true
+		}
+		prefix := name + "="
+		if len(arg) > len(prefix) && arg[:len(prefix)] == prefix {
+			return arg[len(prefix):], true
+		}
+	}
+	return "", false
+}
