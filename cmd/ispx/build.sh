3d426be9e445681e96a7c41b60c08e54f7224708
joeykchen <466719968@qq.com>
2026-04-23 10:04:29 +0800

    chore: remove go generate from ispx build

--- cmd/ispx/build.sh
+++ cmd/ispx/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# Generate code before building
-(cd ../../pkg/ispx && go generate)
-
 # Build the WASM binary
 GOOS=js GOARCH=wasm go build -trimpath -ldflags "-s -w" -o ispx.wasm
