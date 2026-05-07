aa39c584b44e11b174e86b6cfd4a729fcc403855
joeykchen <466719968@qq.com>
2026-05-07 16:57:20 +0800

    chore: add missing license headers (#1548)

--- cmd/spxrunner/main.go
+++ cmd/spxrunner/main.go
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (c) 2024 The XGo Authors (xgo.dev). All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // Package main implements the spxrunner command for running SPX PC projects.
 // This is the command-package entry used by xgo's generic runner directive.
 package main
