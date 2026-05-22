a6c93609789a3c2730078b7c95449132fe387bd5
joeykchen <466719968@qq.com>
2026-05-22 15:16:57 +0800

    chore(release): add SPX v2.0.2 runtime mapping

--- internal/release/release_meta.go
+++ internal/release/release_meta.go
@@ -52,6 +52,7 @@ type releaseVersionMapping struct {
 var releaseVersionMappings = []releaseVersionMapping{
 	{"v2.0.0", "2.2.0"},
 	{"v2.0.1", "2.2.1"},
+	{"v2.0.2", "2.2.2"},
 }
 
 func newReleaseMeta(mapping releaseVersionMapping) ReleaseMeta {
