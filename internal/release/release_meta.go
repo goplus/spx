bc4d7bd8bb66dc6e603e7ed572471cadf7f000bc
joeykchen <466719968@qq.com>
2026-05-28 16:55:11 +0800

    chore(release): add v2.0.3 release metadata mapping (#1604)
    * chore(release): add v2.0.3 version mapping
    
    * test(release): update latest release metadata assertions

--- internal/release/release_meta.go
+++ internal/release/release_meta.go
@@ -53,6 +53,7 @@ var releaseVersionMappings = []releaseVersionMapping{
 	{"v2.0.0", "2.2.0"},
 	{"v2.0.1", "2.2.1"},
 	{"v2.0.2", "2.2.2"},
+	{"v2.0.3", "2.2.3"},
 }
 
 func newReleaseMeta(mapping releaseVersionMapping) ReleaseMeta {
